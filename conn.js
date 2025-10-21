eed5f9be263a645bb4bc251ec02134b6ffedfc24
dattapadre <dattapadre7249@gmail.com>
2025-10-21 14:22:12 +0530

    connection

--- conn.js
+++ conn.js
@@ -3,10 +3,10 @@ const util = require("util");
 const mysql = require("mysql");
 
 const conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"App_demo_Api"
+    host:"bpgfufgwaio3r9syvurk-mysql.services.clever-cloud.com",
+    user:"uw2o79u5rurzlnwn",
+    password:"3h9Fgdz2LpoNV1oChpMv",
+    database:"bpgfufgwaio3r9syvurk"
 });
 
 const exe = util.promisify(conn.query).bind(conn);
